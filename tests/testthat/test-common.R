ad70a7d438ec15780ec7b0974a72d98840bac473
Sabir Meah <smeah@umich.edu>
2022-11-23 23:17:35 -0500

    Added additional test cases

--- tests/testthat/test-common.R
+++ tests/testthat/test-common.R
@@ -1,9 +1,18 @@
 library(twoxtwoxkTables, quietly = TRUE, warn.conflicts = FALSE)
 library(testthat, quietly = TRUE, warn.conflicts = FALSE)
 library(stats, quietly = TRUE, warn.conflicts = FALSE)
+
 data("Titanic")
 partial_tables <- margin.table(Titanic, c(2,4,1))
+
+a <- array(c(1,3,5,6,
+             2,7,3,1,
+             2,4,9,2),
+           dim = c(2,2,3))
+
 test_that("common odds ratios", {
   expect_equal(common.odds.ratio(partial_tables),
                as.numeric(mantelhaen.test(partial_tables)$estimate))
+  expect_equal(common.odds.ratio(a),
+               as.numeric(mantelhaen.test(a)$estimate))
 })
--- tests/testthat/test-cond.R
+++ tests/testthat/test-cond.R
@@ -1,8 +1,15 @@
 library(twoxtwoxkTables, quietly = TRUE, warn.conflicts = FALSE)
 library(testthat, quietly = TRUE, warn.conflicts = FALSE)
+
 data("Titanic")
 partial_tables <- margin.table(Titanic, c(2,4,1))
+
+a <- array(c(1,3,5,6),
+           dim = c(2,2))
+
 test_that("conditional odds ratios", {
   expect_equal(cond.odds.ratios(partial_tables),
                as.numeric(apply(partial_tables, 3, samplesizeCMH::odds.ratio)))
+  expect_equal(cond.odds.ratios(a),
+               as.numeric(samplesizeCMH::odds.ratio(a)))
 })
